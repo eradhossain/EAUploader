cc9b2548d33d613f15c1427f0c3603af593d498d
Erad Hossain <98438334+eradhossain@users.noreply.github.com>
2024-11-07 10:58:44 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello https://t.me/EaUploaderBot'
+    return 'Hello <a href="https://t.me/EaUploaderBot">@EaUploaderBot</a>'
 
 
 if __name__ == "__main__":
